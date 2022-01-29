a3c987bb07382b8e765e8f8dc86274b44c1ee026
Shánenoi <49734716+shanenoi@users.noreply.github.com>
2022-01-29 20:05:34 +0700

    Setup Config (#2)
    * setup permission
    
    * Refactor mod app
    
    * Fix some issues from Github Actions

---
+++ src/crab/mod.rs
@@ -0,0 +1,6 @@
+pub mod app;
+pub mod config;
+
+static BASE_PATH: &str = "/tmp";
+// static DEFAULT_CONFIG: &'static str = "default.yml";
+static DEFAULT_FILEMOD: u32 = 0o755;
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,10 @@
-use std::{
-    collections::BTreeMap,
-    fs::File,
-    io::Write,
-    path::{ Path, PathBuf },
-    os::unix::fs::PermissionsExt
-};
+mod crab;
+use crab::app::{ App, AppsConfig };
+use std::fs::File;
 
-type AppsConfig = BTreeMap<String, BTreeMap<String, String>>;
-
-static DEFAULT_CONFIG: &'static str = "default.yml";
-static BASE_PATH: &'static str = "/tmp";
-static ERROR_CONFIG: &'static str = "[ERR] Load the config file: ";
-static ERROR_CONFIG_FILE: &'static str = "[ERR] config file: ";
-
-#[derive(Debug)]
-pub struct App {
-    pub name: String,
-    pub content: String,
-}
-
-impl App {
-    pub fn new(config: &BTreeMap<String, String>) -> Vec<Box<App>> {
-        let mut result = Vec::new();
-        for (key, value) in &*config {
-            result.push(Box::new(App {
-                name: key.to_string(),
-                content: value.to_string(),
-            }));
-        }
-        return result;
-    }
-
-    pub fn abs_path(&self) -> PathBuf {
-        Path::new(BASE_PATH).join(&self.name)
-    }
-
-    pub fn save(&self) {
-        let mut file = File::create(self.abs_path()).unwrap();
-        file.write_all(self.content.as_bytes()).unwrap();
-        file.sync_all().unwrap();
-    }
-}
+static DEFAULT_CONFIG: &str = "default.yml";
+static ERROR_CONFIG: &str = "[ERR] Load the config file: ";
+static ERROR_CONFIG_FILE: &str = "[ERR] config file: ";
 
 fn main() -> std::io::Result<()> {
     let file_config = File::open(DEFAULT_CONFIG);
@@ -51,14 +15,12 @@ fn main() -> std::io::Result<()> {
                 serde_yaml::from_reader(&config);
             match deserialized_map {
                 Ok(map) => {
-                    match map.get("apps") {
-                        Some(value) => {
-                            let apps = App::new(value);
-                            for app in apps {
-                                app.save();
-                            }
+                    if let Some(value) = map.get("apps") {
+                        let apps = App::new(value);
+                        for app in apps {
+                            app.save();
+                            println!("installed to {:?}", app.abs_path());
                         }
-                        None => {}
                     }
                     Ok(())
                 }
