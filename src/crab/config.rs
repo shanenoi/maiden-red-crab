83a79fa503e5854376451948ed432807113d1233
shanenoi <shanenoi.org@gmail.com>
2022-02-03 23:27:54 +0700

    Add base function

--- src/crab/config.rs
+++ src/crab/config.rs
@@ -1,26 +1,26 @@
-// use crate::crab::{
-//     app::AppsConfig,
-//     BASE_PATH,
-// };
+use crate::crab::{
+    app::AppsConfig,
+    BASE_PATH,
+};
 
-// pub struct Config {
-//     apps_path:           &'static str,
-//     executable_dir_path: &'static str,
-// }
-// 
-// impl Config {
-//     pub fn new() -> Box<Config> {
-//         Box::new(Config {
-//             apps_path: "",
-//             executable_dir_path: "",
-//         })
-//     }
-// 
-//     pub fn source(&self) {}
-// 
-//     pub fn execute_path(&self) {}
-// 
-//     pub fn export_shellrc(&self) {}
-// 
-//     fn init_config(&self) {}
-// }
+pub struct Config {
+    apps_path:           &'static str,
+    executable_dir_path: &'static str,
+}
+
+impl Config {
+    pub fn new() -> Box<Config> {
+        Box::new(Config {
+            apps_path: "",
+            executable_dir_path: "",
+        })
+    }
+
+    pub fn source(&self) {}
+
+    pub fn execute_path(&self) {}
+
+    pub fn export_shellrc(&self) {}
+
+    fn init_config(&self) {}
+}
