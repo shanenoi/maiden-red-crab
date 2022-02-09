9af958ebc4b5660511a719fa13ad48f3ad364715
Shánenoi <49734716+shanenoi@users.noreply.github.com>
2022-02-09 23:36:47 +0700

    Refactor Config mod (#7)
    * Add base function
    
    * Refactor config interface
    
    * Fix and impl config functions
    
    * Fix lint
    
    * Add quickfix config

--- src/crab/config.rs
+++ src/crab/config.rs
@@ -1,26 +1,110 @@
-// use crate::crab::{
-//     app::AppsConfig,
-//     BASE_PATH,
-// };
+use crate::crab::{
+    APP_CONFIG_DIR,
+    APP_CONFIG_TEMPLATE,
+    CRAB_CONFIG,
+    CRAB_CONFIG_TEMPLATE,
+    ERROR_CONFIG_FILE,
+    ERROR_CREATE_APP_CONFIG_DIR,
+    ERROR_FIND_CONFIG_DIR,
+    PKG_NAME,
+    app::AppsConfig,
+};
+use dirs;
+use regex::Regex;
+use serde::de::Error;
+use std::io::Write;
+use std::path::{ PathBuf };
+use std::{fs, fs::File };
 
-// pub struct Config {
-//     apps_path:           &'static str,
-//     executable_dir_path: &'static str,
-// }
-// 
-// impl Config {
-//     pub fn new() -> Box<Config> {
-//         Box::new(Config {
-//             apps_path: "",
-//             executable_dir_path: "",
-//         })
-//     }
-// 
-//     pub fn source(&self) {}
-// 
-//     pub fn execute_path(&self) {}
-// 
-//     pub fn export_shellrc(&self) {}
-// 
-//     fn init_config(&self) {}
-// }
+pub struct Config {
+    #[allow(dead_code)]
+    apps_path:           &'static str,
+
+    #[allow(dead_code)]
+    executable_dir_path: &'static str,
+}
+
+pub trait Init {
+    fn init(&self) -> PathBuf;
+    fn create_apps_config(&self) -> PathBuf;
+    fn load_apps_config(&self) -> Result<AppsConfig, serde_yaml::Error>;
+    fn create_crab_config(&self) -> PathBuf;
+    fn load_crab_config(&self);
+}
+
+pub trait Export {
+    fn read_shellrc(&self) -> Vec<String>;
+    fn check_shellrc(&self) -> bool;
+    fn export(&self);
+}
+
+impl Config {
+    pub fn new() -> Box<Config> {
+        Box::new(Config {
+            apps_path: "",
+            executable_dir_path: "",
+        })
+    }
+
+    fn init_file(filepath: &PathBuf, content: &[u8]) {
+        if !filepath.exists() {
+            let mut file = File::create(filepath).unwrap();
+            file.write_all(content).unwrap();
+            file.sync_all().unwrap();
+        }
+    }
+}
+
+impl Init for Config {
+    fn init(&self) -> PathBuf {
+        let default_config_dir: PathBuf;
+
+        if let Some(config_dir) = dirs::config_dir() {
+            default_config_dir = config_dir.join(PKG_NAME);
+            if let Err(_) = fs::create_dir_all(&default_config_dir) {
+                panic!("{}", ERROR_CREATE_APP_CONFIG_DIR);
+            }
+        } else {
+                panic!("{}", ERROR_FIND_CONFIG_DIR);
+        }
+
+        default_config_dir
+    }
+
+    fn create_apps_config(&self) -> PathBuf {
+        let config = self.init().join(APP_CONFIG_DIR);
+        Config::init_file(&config, APP_CONFIG_TEMPLATE.as_bytes());
+        config
+    }
+
+    fn create_crab_config(&self) -> PathBuf {
+        let init_ = self.init();
+        let config = init_.join(CRAB_CONFIG);
+
+        let crab_config = Regex::new(r"(<CRAB_CONFIG>)").unwrap();
+        let home = Regex::new(r"(<HOME>)").unwrap();
+        let crab_config_path = (&init_).to_str().unwrap();
+        let home_path = dirs::home_dir().unwrap();
+
+        Config::init_file(
+            &config,
+            home.replace_all(
+                &crab_config.replace_all(CRAB_CONFIG_TEMPLATE, crab_config_path),
+                home_path.to_str().unwrap()
+            ).as_bytes()
+        );
+
+        config
+    }
+
+    fn load_apps_config(&self) -> Result<AppsConfig, serde_yaml::Error> {
+        let file_config = File::open(self.create_apps_config());
+        if let Ok(file) = file_config {
+            serde_yaml::from_reader(file)
+        } else {
+            Err(serde_yaml::Error::custom(ERROR_CONFIG_FILE))
+        }
+    }
+
+    fn load_crab_config(&self) {}
+}
--- src/crab/mod.rs
+++ src/crab/mod.rs
@@ -1,6 +1,27 @@
 pub mod app;
 pub mod config;
 
+static APP_CONFIG_DIR: &str = "apps.yml";
+static CRAB_CONFIG: &str = "crabs.yml";
 static BASE_PATH: &str = "/tmp";
-// static DEFAULT_CONFIG: &'static str = "default.yml";
 static DEFAULT_FILEMOD: u32 = 0o755;
+
+#[allow(dead_code)]
+static ERROR_CONFIG: &str = "[ERR] Load the config file: ";
+
+static ERROR_CONFIG_FILE: &str = "[ERR] config file: ";
+static ERROR_CREATE_APP_CONFIG_DIR: &str = "[ERR] Can't create app config dir";
+static ERROR_FIND_CONFIG_DIR: &str = "[ERR] Can't find config dir";
+static PKG_NAME: &str = env!("CARGO_PKG_NAME");
+
+static APP_CONFIG_TEMPLATE: &str = "apps:
+  vim.md: google-chrome-stable --app=https://vim.md/
+  messenger: google-chrome-stable --app=https://messenger.com
+  keep_g: google-chrome-stable --app=https://keep.google.com/
+  zalo: google-chrome-stable --app=https://chat.zalo.me
+";
+
+static CRAB_CONFIG_TEMPLATE: &str = "crab:
+  apps_path: <CRAB_CONFIG>/apps.yml
+  executable_dir_path: <HOME>/.local/bin
+";
